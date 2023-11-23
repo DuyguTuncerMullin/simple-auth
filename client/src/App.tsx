2d134521feea77ee2a74e4fcd01ac7259b6c500b
Duygu Tuncer Mullin <duygu.tuncer001@gmail.com>
2023-11-22 20:05:43 -0600

    add login and logout features, plus welcome page, set the simple react-routers for navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/api/register" element={<Register />} />
+          <Route path="/api/login" element={<Login />} />
+          <Route path="/api/welcome" element={<Welcome />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Register.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Register: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const inputFiledHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const submitHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { data } = await axios.post(`http://localhost:4000/api/register`, {
+      name,
+      email,
+      password,
+    });
+    console.log("data", data);
+    if (data.success) {
+      navigate("/api/welcome");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Register Info</h1>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            name="name"
+            placeholder={"name"}
+            value={name}
+            onChange={inputFiledHandler}
+          ></input>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            value={email}
+            onChange={inputFiledHandler}
+          ></input>
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            value={password}
+            onChange={inputFiledHandler}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
